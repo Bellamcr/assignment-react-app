97aa653aa1ac1529f5dd952bf0ed92b67947af3d
Bellamcr <izabella.mcr@gmail.com>
2023-11-20 18:35:04 -0700

    Increase the counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCount(c => c + 1); // ✅ Pass a state updater
+    }, 3000);
+    return () => clearInterval(intervalId);
+  }, []); // ✅ Now count is not a dependency
+
 
   return (
     <>     
