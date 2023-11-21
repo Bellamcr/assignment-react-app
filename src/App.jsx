7c95ef15f6bc5b6f2cb8fa573f7597bf82401228
Bellamcr <izabella.mcr@gmail.com>
2023-11-20 19:23:26 -0700

    Formating

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState, useEffect} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
-    console.log("useEffect starting")
+    console.log("useEffect starting");
     const intervalId = setInterval(() => {
-      setCount(c => c + 1); 
+      setCount((c) => c + 1);
     }, 3000);
     return () => clearInterval(intervalId);
-  }, []); 
-
+  }, []);
 
   return (
-    <>     
+    <>
       <h1>Counter</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-      </div>  
+      </div>
       <div>
-        <button onClick={() => setCount((count) => count = 0)}>
-          Reset
-        </button>
-      </div>    
+        <button onClick={() => setCount((count) => (count = 0))}>Reset</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
